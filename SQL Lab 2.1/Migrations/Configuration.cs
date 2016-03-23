9be43d1645ce6141f222e20da4939cf668c94b55
kvnmach <kvnmach@gmail.com>
2016-03-22 22:49:38 -0500

    added tasks

--- SQL Lab 2.1/Migrations/Configuration.cs
+++ SQL Lab 2.1/Migrations/Configuration.cs
@@ -125,42 +125,44 @@ namespace SQL_Lab_2._1.Migrations
 
             var time3 = new TimeEntry()
             {
-                Date = new DateTime(1000, 12, 01),
-                TimeSpent = 1.0f,
-                Developer = seth,
+                Date = new DateTime(3000, 12, 01),
+                TimeSpent = 3.0f,
+                Developer = teee,
                 Task = new Task()
                 {
-                    Name = "1",
+                    Name = "3",
                     Project = firstproj
                 }
 
             };
 
-            var time1 = new TimeEntry()
+            var time4 = new TimeEntry()
             {
-                Date = new DateTime(1000, 12, 01),
-                TimeSpent = 1.0f,
+                Date = new DateTime(4000, 12, 01),
+                TimeSpent = 4.0f,
                 Developer = seth,
                 Task = new Task()
                 {
                     Name = "1",
-                    Project = firstproj
+                    Project = secproj
                 }
 
             };
 
-            var time1 = new TimeEntry()
+            var time5 = new TimeEntry()
             {
-                Date = new DateTime(1000, 12, 01),
-                TimeSpent = 1.0f,
-                Developer = seth,
+                Date = new DateTime(5000, 12, 01),
+                TimeSpent = 5.0f,
+                Developer = kevin,
                 Task = new Task()
                 {
-                    Name = "1",
-                    Project = firstproj
+                    Name = "5",
+                    Project = secproj
                 }
 
             };
+
+            context.Industries.AddOrUpdate(b => b.IndustryComments, industry1);
         }
 
     }
